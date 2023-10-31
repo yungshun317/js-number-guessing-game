29cee3173f71af751df4b3f80c3ef770e7156d07
yungshun317 <seraph317@gmail.com>
2023-10-31 19:46:12 +0800

    [FEAT] Compare situations and manipulate elements

--- script.js
+++ script.js
@@ -23,5 +23,43 @@ document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     // 36 number
+
+    // When there is no input
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number';
+        // When player wins
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct number';
+        document.querySelector('.number').textContent = secretNumber;
+
+        // Manipulate CSS styles
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+        // When guess is too high
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too high';
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+        // When guess is too low
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too low';
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
 });
 
