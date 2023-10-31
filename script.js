b5ac2b46e1191e0681ba077c9bffe217c2d7b2f3
yungshun317 <seraph317@gmail.com>
2023-10-31 19:37:32 +0800

    [FEAT] Select elements and handle click events

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+// Start Guessing...
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 16;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 36;
+console.log(document.querySelector('.guess').value);
+// 36
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    // 36 number
+});
+
