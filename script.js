ba3e87c1b9302c972838270cff71c01480661fcb
yungshun317 <seraph317@gmail.com>
2023-11-01 12:50:32 +0800

    [REFACTOR] Extract abstractions

--- script.js
+++ script.js
@@ -47,11 +47,13 @@ document.querySelector('.check').addEventListener('click', function() {
         // When guess is too high
     } else if (guess !== secretNumber) {
        if (score > 1) {
-           document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!' : 'Too Low!';
+           // document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!' : 'Too Low!';
+           displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
            score--;
            document.querySelector('.score').textContent = score;
        } else {
-           document.querySelector('.message').textContent = 'You lost the game';
+           // document.querySelector('.message').textContent = 'You lost the game';
+           displayMessage('You lost the game');
            document.querySelector('.score').textContent = 0;
        }
     }
@@ -82,7 +84,8 @@ document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-    document.querySelector('.message').textContent = 'Start Guessing';
+    // document.querySelector('.message').textContent = 'Start Guessing...';
+    displayMessage('Start Guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
